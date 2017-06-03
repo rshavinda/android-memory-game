4d11548a819774d418676f035800e921fdef3045
rudmuhandiramge <randhirmuhandiramge@gmail.com>
2017-06-03 21:49:46 +0530

    new layouts and code
    code : playOrExit UI
    layout: star layout

--- app/src/main/java/sliit/mad/g3/mtit_mad/PlayOrExit.java
+++ app/src/main/java/sliit/mad/g3/mtit_mad/PlayOrExit.java
@@ -20,8 +20,46 @@ public class PlayOrExit extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_play_or_exit);
 
+
         btnPlay=(ImageView)findViewById(R.id.btn_play);
         btnExit=(ImageView)findViewById(R.id.btn_Exit);
+
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PlayOrExit.this,MainActivity.class));
+            }
+        });
+
+
+
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder adb=new AlertDialog.Builder(PlayOrExit.this);
+
+                final AlertDialog.Builder builder = adb.setMessage(" Are you sure you want to quit?")
+                        .setTitle("Exit")
+                        .setNeutralButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setCancelable(false)
+                        .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                AlertDialog alertDialog=adb.create();
+                alertDialog.show();
+            }
+        });
+
 
 
 
